98e414d1e8e7915194099971d805653c18639d7b
Manuela Masjoan <manuelamasjoan@MacBook-Air-de-Manuela-2.local>
2022-05-03 16:39:15 -0300

    first commit

---
+++ src/test/java/com/example/jibberjabber/JibberJabberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jibberjabber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibberJabberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
